7001d13f52d8046ea195edf5d60eb80289446170
salt-mountain <48201364+salt-mountain@users.noreply.github.com>
2021-12-02 17:49:49 -0500

    Fix

--- 2021/01/solution.py
+++ 2021/01/solution.py
@@ -2,11 +2,7 @@
 
 # Part 1
 depths = [int(line.strip()) for line in open("input.txt", "r")]
-print(
-    sum(
-        [1 for i in range(1, len(depths)) if depths[i] > depths[i-1]]
-        )
-    )
+print(sum([1 for i in range(1, len(depths)) if depths[i] > depths[i-1]]))
 
 # Part 2
 windows = [depths[i: i + 3] for i in range(len(depths) - 3 + 1)]
