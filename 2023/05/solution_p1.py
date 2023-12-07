56afd192e4f01b9f98d5f922ac4bac7ca4308c11
salt-mountain <goran.osim@gmail.com>
2023-12-07 00:42:39 -0500

    Fix

--- 2023/05/solution_p1.py
+++ 2023/05/solution_p1.py
@@ -6,21 +6,20 @@ class SeedMap:
     def __init__(self, from_map_type, target_map_type):
         self.from_map_type = from_map_type
         self.target_map_type = target_map_type
-        # tuple is (source, max_source, target)
-        # source is inclusive, max_source is exclusive
         self.map_values: list[tuple[int, int, int]] = []
 
     def add(self, target, source, offset):
         self.map_values.append((source, source + offset, target))
 
     def get_destination(self, seed_value):
-        # for each mapping range
+        # For a given seed
+        # For each mapping range
         for source, max_source, target in self.map_values:
             # if seed_value is between range
             if source <= seed_value < max_source:
-                # map it
+                # Return the location value
                 return target + (seed_value - source)
-        # if no range was found return values unchanged
+        # Any source numbers that aren't mapped correspond to the same destination number.
         return seed_value
     
     def __str__(self):
@@ -38,30 +37,30 @@ def main():
     # Create Seed Maps
     seed_maps: list[SeedMap] = []
 
-    # for each line after the first
+    # For each line after the first
     for line in lines[1:]:
         # skip any empty lines
         if line == "":
             continue
         
-        # if new map declaration
+        # New Map
         if line.endswith("map:"):
             # get the map type we're going from and
-            # the map type we're going to
             from_map_type, to_map_type = line.split(" ")[0].split("-to-", 1)
-            # create the map and add it to the list
+            # Create the map and add it to the list
             current_map = SeedMap(from_map_type, to_map_type)
             seed_maps.append(current_map)
         else:
-            # if not a map declaration, add the mapping
-            # the * unpacks the list into 3 arguments
+            # if it's not a new map, add the map values
             current_map.add(*[int(s) for s in line.split(" ")])
     
     # Start high, the input values are huge
     minimum_location = sys.maxsize
-
+    
     for seed in seeds:
-        # map it through all the seed maps
+        # For each seed
+        # Map it through all the seed maps
+        # Get the location number
         for seed_map in seed_maps:
             seed = seed_map.get_destination(seed)
         # update the minimum location
