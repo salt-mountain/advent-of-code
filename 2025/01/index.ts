e10af1b1c7a7a3860d1fabb6032719b4aea11009
salt-mountain <48201364+salt-mountain@users.noreply.github.com>
2025-12-03 10:49:53 -0500

    Learning Comment

--- 2025/01/index.ts
+++ 2025/01/index.ts
@@ -1,5 +1,8 @@
 import * as fs from 'fs';
 
+// Stuck to TypeScript docs so I read it this way as opposed to
+// using a nice Bun built in that I learned about after.
+// Leaving this in to show learn progress.
 const filePath: string = 'input.txt';
 const content: string = fs.readFileSync(filePath, 'utf-8');
 const lines: string[] = content.split(/\r?\n/);
@@ -11,7 +14,7 @@ let clicks = 0;
 for (const line of lines) {
     const direction = line.startsWith("R") ? 1 : -1;
     const turn = parseInt(line.slice(1));
-    
+
     // Count how many times we cross 0 during this turn
     for (let i = 0; i < turn; i++) {
         dial = (dial + direction + 100) % 100;
@@ -19,8 +22,8 @@ for (const line of lines) {
             clicks += 1;
         }
     }
-    
-    if(dial === 0){
+
+    if (dial === 0) {
         zeros += 1;
     }
 }
