6a10852aca5027f8e4adc46d22a101aac75eb7ae
salt-mountain <48201364+salt-mountain@users.noreply.github.com>
2025-12-03 10:26:27 -0500

    2025 Day 01

--- 2025/01/index.ts
+++ 2025/01/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import * as fs from 'fs';
+
+const filePath: string = 'input.txt';
+const content: string = fs.readFileSync(filePath, 'utf-8');
+const lines: string[] = content.split(/\r?\n/);
+
+let dial = 50;
+let zeros = 0;
+let clicks = 0;
+
+for (const line of lines) {
+    const direction = line.startsWith("R") ? 1 : -1;
+    const turn = parseInt(line.slice(1));
+    
+    // Count how many times we cross 0 during this turn
+    for (let i = 0; i < turn; i++) {
+        dial = (dial + direction + 100) % 100;
+        if (dial === 0) {
+            clicks += 1;
+        }
+    }
+    
+    if(dial === 0){
+        zeros += 1;
+    }
+}
+
+console.log(zeros);
+console.log(clicks);
